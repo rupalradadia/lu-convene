992d2835e81c287710d2722b8cb63c1af4e20e1a
Moushmi Sreekanthan <moush1997@gmail.com>
2021-09-13 16:58:45 -0500

    Trial

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hello world
+          Learn React hello project!
         </a>
       </header>
     </div>
