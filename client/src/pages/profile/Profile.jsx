e63b6aa22dd89958e7ced3a6d032649a12a110b4
DESKTOP-PVETJ1P\denis <rupal.radadia@gmail.com>
2021-10-13 16:11:39 -0500

    apis added

---
+++ client/src/pages/profile/Profile.jsx
@@ -0,0 +1,51 @@
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Feed from "../../components/feed/Feed";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router";
+
+export default function Profile() {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState({});
+  const username = useParams().username;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?username=${username}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [username]);
+
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="/assets/person/noCover.png"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="/assets/person/noAvatar.png"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">{user.username}</h4>
+              <span className="profileInfoDesc">{user.desc}</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed username={username} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
